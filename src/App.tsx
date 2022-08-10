97bfc54c413a7ad5be0d650048bcf4b1b8b2e237
adiletelf <adiletelf@gmail.com>
2022-08-10 21:13:36 +0600

    Create initial structure and Navbar component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Navbar } from './components'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/logo.png'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar-logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className="navbar-links">
+        <h2><a href='#home' className='hover-underline-animation'>Home</a></h2>
+        <h2><a href='#work' className='hover-underline-animation'>Work</a></h2>
+        <h2><a href='#about' className='hover-underline-animation'>About</a></h2>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
