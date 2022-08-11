514fb8e36fc540d1e286691b803eeef2fe4c475f
adiletelf <adiletelf@gmail.com>
2022-08-11 17:36:45 +0600

    Colorize Clients images on hover

--- src/components/clients/Clients.tsx
+++ src/components/clients/Clients.tsx
@@ -1,5 +1,24 @@
+import React from 'react'
+import {
+  airbnb, airbnbColored,
+  fedex, fedexColored,
+  google, googleColored,
+  microsoft, microsoftColored
+} from './imports'
 import './clients.css'
-import { airbnb, fedex, google, microsoft } from './imports'
+
+const handleImage = (image: string, coloredImage: string) => {
+  return {
+    src: image,
+    onMouseMove: (e: React.MouseEvent<HTMLImageElement>) => { e.currentTarget.src = coloredImage },
+    onMouseOut: (e: React.MouseEvent<HTMLImageElement>) => { e.currentTarget.src = image }
+  }
+}
+
+const airbnbProps = handleImage(airbnb, airbnbColored)
+const fedexProps = handleImage(fedex, fedexColored)
+const googleProps = handleImage(google, googleColored)
+const microsoftProps = handleImage(microsoft, microsoftColored)
 
 const Clients = () => {
   return (
@@ -8,10 +27,10 @@ const Clients = () => {
         <h2><span>Some of the</span> clients I have designed for</h2>
       </div>
       <div className='clients-images'>
-        <img src={airbnb} alt='airbnb' />
-        <img src={fedex} alt='fedex' />
-        <img src={google} alt='google' />
-        <img src={microsoft} alt='microsoft' />
+        <img {...airbnbProps} alt='airbnb' />
+        <img {...fedexProps} alt='fedex' />
+        <img {...googleProps} alt='google' />
+        <img {...microsoftProps} alt='microsoft' />
       </div>
     </div>
   )
--- src/components/clients/imports.ts
+++ src/components/clients/imports.ts
@@ -1,11 +1,15 @@
 import airbnb from '../../assets/logos/airbnb.png'
+import airbnbColored from '../../assets/logos/airbnb-colored.png'
 import fedex from '../../assets/logos/fedex.png'
+import fedexColored from '../../assets/logos/fedex-colored.png'
 import google from '../../assets/logos/google.png'
+import googleColored from '../../assets/logos/google-colored.png'
 import microsoft from '../../assets/logos/microsoft.png'
+import microsoftColored from '../../assets/logos/microsoft-colored.png'
 
 export {
-  airbnb,
-  fedex,
-  google,
-  microsoft
+  airbnb, airbnbColored,
+  fedex, fedexColored,
+  google, googleColored,
+  microsoft, microsoftColored
 }
